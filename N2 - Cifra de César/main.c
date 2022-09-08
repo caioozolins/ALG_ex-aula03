978aefddbc2421fb53801cb29495e00bf3da9bba
Caio Ozolins Mendes <caio.mendes@alunos.unis.edu.br>
2022-09-08 14:39:29 -0300

    N2 - Cifra de César
    Created function strupper
    Created struct Words

--- N2 - Cifra de César/main.c
+++ N2 - Cifra de César/main.c
@@ -1,20 +1,41 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <locale.h>
+
+// converte uma string em maiúsculas
+void strupper(char s1[]){
+  int j=0;
+  while (s1[j] != '\0'){
+    s1[j] = toupper(s1[j]);
+    j++;
+  }
+  s1[j] = '\0'; //carácter que indica o fim da string
+}
 
 int main(){
   setlocale(LC_ALL, "portuguese");
 
-  int cases, cipher;
-  char word[51];
+  int cases, i, n=1;
+  typedef struct{
+    char word[51];
+    int cipher;
+  } Words;
   printf("Digite a quantidade de palavras: ");
   scanf("%d", &cases);
-  for (cases; cases > 0; cases--)
-  {
-    printf("\nInsira a palavra com todas as letras maiúsculas: ");
-    scanf("%s", &word);
-    printf("- - %s", word);
+  Words Case[cases];
+  i = cases;
+  for (cases; cases > 0; cases--){
+    printf("\nInsira a %dª palavra: ", n);
+    scanf("%s", &Case[cases].word);
+    printf("Insira a quantidade de casa deslocadas: ");
+    scanf("%d", &Case[cases].cipher);
+    strupper(Case[cases].word);
+    n++;
   }
-  
+  for (i; i > 0; i--){
+    printf("\n- %s\n", Case[i].word);
+    printf("- %d", Case[i].cipher);
+  }
 
   return 0;
 }

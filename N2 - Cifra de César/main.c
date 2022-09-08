94a45c5aa628b3cf7d44a9dc70ebd66823b675a1
Caio Ozolins Mendes <caio.mendes@alunos.unis.edu.br>
2022-09-08 17:08:25 -0300

    N2 - Cifra de César
    Created function cesar

--- N2 - Cifra de César/main.c
+++ N2 - Cifra de César/main.c
@@ -12,13 +12,29 @@ void strupper(char s1[]){
   s1[j] = '\0'; //carácter que indica o fim da string
 }
 
+// Cifra de César
+void cesar(char s1[], int k){
+  int c=0;
+  while (s1[c] != '\0'){
+    s1[c] -= k;
+    if((s1[c]) > 90){
+      s1[c] -= 26;
+    }
+    if((s1[c]) < 65){
+      s1[c] += 26;
+    }
+    c++;
+  }
+  s1[c] = '\0';
+}
+
 int main(){
   setlocale(LC_ALL, "portuguese");
 
   int cases, i, n=1;
   typedef struct{
     char word[51];
-    int cipher;
+    int key;
   } Words;
   printf("Digite a quantidade de palavras: ");
   scanf("%d", &cases);
@@ -27,14 +43,16 @@ int main(){
   for (cases; cases > 0; cases--){
     printf("\nInsira a %dª palavra: ", n);
     scanf("%s", &Case[cases].word);
-    printf("Insira a quantidade de casa deslocadas: ");
-    scanf("%d", &Case[cases].cipher);
+    printf("Insira a chave (nº entre 0 e 25): ");
+    scanf("%d", &Case[cases].key);
     strupper(Case[cases].word);
     n++;
   }
   for (i; i > 0; i--){
     printf("\n- %s\n", Case[i].word);
-    printf("- %d", Case[i].cipher);
+    printf("- %d", Case[i].key);
+    cesar(Case[i].word, Case[i].key);
+    printf("\n- - %s", Case[i].word);
   }
 
   return 0;
